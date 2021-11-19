56083230c0ffb7a723bca0675af13b8a323395db
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-19 11:46:37 +0800

    lowest(number1, number2, number3)

--- LowestNumber.py
+++ LowestNumber.py
@@ -14,3 +14,12 @@ def highest(number1, number2, number3):
     else:
         largestNum = number3
     print("The largest of the 3 numbers is : ", largestNum)
+
+def lowest(number1, number2, number3):
+    if (number1 < number2) and (number1 < number3):
+        smallestNum = number1
+    elif (number2 < number1) and (number2 < number3):
+        smallestNum = number2
+    else:
+        smallestNum = number3
+    print("The smallest of the 3 numbers is : ", smallestNum)
