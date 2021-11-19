472dbf61d3aa294442e3ec12847def9a28e30173
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-19 11:45:22 +0800

    insertNumber()

---
+++ LowestNumber.py
@@ -0,0 +1,8 @@
+def insertNumber():
+    global number1
+    global number2
+    global number3
+    number1 = int(input('Enter First number : '))
+    number2 = int(input('Enter Second number : '))
+    number3 = int(input('Enter Third number : '))
+
